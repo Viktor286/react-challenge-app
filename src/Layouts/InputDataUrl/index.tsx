ffacb31411b613009f714ad934c7bbc849262251
viktor286 <just.viktor@gmail.com>
2022-07-30 15:41:10 -0400

    add mui Container and TextField

--- src/Layouts/InputDataUrl/index.tsx
+++ src/Layouts/InputDataUrl/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { TextField } from '@mui/material';
 
 export default function InputDataUrlLayout() {
-  return <>InputDataUrlLayout</>;
+  return (
+    <TextField
+      fullWidth
+      sx={{ mt: 4, mb: 4 }}
+      label="Json Data Url"
+      placeholder="Json Data Url"
+      defaultValue="https://data.nasa.gov/resource/y77d-th95.json"
+      disabled={false}
+      error={false}
+      helperText="Insert data url. Returning data MUST be an array json with each element is key/value pair."
+      // onChange={}
+    />
+  );
 }
--- src/Layouts/MainPage/index.tsx
+++ src/Layouts/MainPage/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Typography } from '@mui/material';
 
 interface IMainPageLayoutProps {
   children: React.ReactNode;
 }
 
 export default function MainPageLayout({ children }: IMainPageLayoutProps) {
-  return <>MainPageLayout {children}</>;
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', mt: 2, mb: 2 }}>
+        <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+          Condition Builder
+        </Typography>
+        {children}
+      </Container>
+    </>
+  );
 }
